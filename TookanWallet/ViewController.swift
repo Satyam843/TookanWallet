5971d8dc9875f1a7e143b011772c20576dd019ff
satyam <satyam.kumar@jungleworks.com>
2020-08-05 11:38:45 +0530

    Initial Commit

---
+++ TookanWallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TookanWallet
+//
+//  Created by soc-admin on 8/5/20.
+//  Copyright © 2020 soc-admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
