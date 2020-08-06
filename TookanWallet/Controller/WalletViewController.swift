f31ffeda38c7d2392dae25e5d25191ec9eda2d9e
satyam <satyam.kumar@jungleworks.com>
2020-08-06 15:48:23 +0530

    started designing tableview

--- TookanWallet/Controller/WalletViewController.swift
+++ TookanWallet/Controller/WalletViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class WalletViewController: UIViewController {
 
+   
     @IBOutlet weak var tapAddMoney: UIButton!
     @IBOutlet weak var lblLimit: UILabel!
     @IBOutlet weak var lblMoney: UILabel!
     @IBOutlet weak var lblBalance: UILabel!
     @IBOutlet weak var walletTableView: UITableView!
     var cellHeight = [100,140,140,280]
+    var sectionTitles  = ["5 August 2020","4 August 2020"]
     override func viewDidLoad() {
         super.viewDidLoad()
 super.view.backgroundColor = UIColor.init(red: 28/255, green: 52/255, blue: 248/255, alpha: 255/255)
@@ -40,49 +42,25 @@ extension WalletViewController: UITableViewDelegate,UITableViewDataSource
   
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return sectionTitles[section]
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return 2
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      
-        if(indexPath.row == 0)
-        {
-              let cell = walletTableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! walletViewCell1
-        cell.lblBalanceHeading.text = "your Balance"
-        cell.lblBalanceValue.text = "Rs .  3547"
-        cell.backgroundColor = UIColor.init(red: 68/255, green: 148/255, blue: 243/255, alpha: 1)
-    cell.celldelegate=self
-            cell.index=indexPath
-            return cell
-        }
-            else if(indexPath.row == 2)
-        {
-            let cell = walletTableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! walletViewCell2
-            return cell
-        }
-        else if(indexPath.row == 3)
-        {
-            let cell = walletTableView.dequeueReusableCell(withIdentifier: "cell3", for: indexPath) as! walletViewCell2
-            return cell
-        }
-        else
-        {
-            let cell4 = walletTableView.dequeueReusableCell(withIdentifier: "cell4", for: indexPath) as! walletViewCell4
-            return cell4
-        }
-       
+      let cell = walletTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75
+    }
+    /*func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return CGFloat(cellHeight[indexPath.row])
-    }
-}
-extension WalletViewController : Wallet
-{
-    func onClick(index: Int) {
-        
-    }
-    
-    
+    }*/
 }
