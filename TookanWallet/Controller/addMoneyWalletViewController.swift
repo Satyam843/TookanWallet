87bff2fa7fb248ff94d1df538e9527892f6780ff
satyam <satyam.kumar@jungleworks.com>
2020-08-07 16:24:52 +0530

    Implemented table view for add Money

--- TookanWallet/Controller/addMoneyWalletViewController.swift
+++ TookanWallet/Controller/addMoneyWalletViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class addMoneyWalletViewController: UIViewController {
-
+    @IBOutlet weak var btnProceed: UIButton!
+    
+    @IBOutlet weak var addMoneyWalletTableView: UITableView!
     @IBOutlet weak var lblMoneyValue: UILabel!
     @IBOutlet weak var lblAddMoneyWallet: UILabel!
+    var bankData = ["State Bank of India","HDFC Bank","ICICI Bank","Add Card"]
+    var accountNumber = ["A/c No . *****56847","A/c No . *****56847","A/c No . *****56847","A/c No . *****56847"]
     override func viewDidLoad() {
         super.viewDidLoad()
         lblAddMoneyWallet.text = " Add Money Wallet"
@@ -23,8 +27,22 @@ class addMoneyWalletViewController: UIViewController {
         //lblAddMoneyWallet.adjustsFontForContentSizeCategory = true
         // Do any additional setup after loading the view.
     }
-    
 
+}
+extension addMoneyWalletViewController:  UITableViewDelegate,UITableViewDataSource
+{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = addMoneyWalletTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! addMoneyWalletCell
+        cell.lblBankName.text = bankData[indexPath.row]
+        cell.lblAccountNo.text = accountNumber[indexPath.row]
+        return cell
+        
+    }
    
-
 }
---
+++ TookanWallet/TableViewCell/addMoneyWalletCell.swift
@@ -0,0 +1,26 @@
+//
+//  addMoneyWalletCell.swift
+//  TookanWallet
+//
+//  Created by soc-admin on 8/7/20.
+//  Copyright © 2020 soc-admin. All rights reserved.
+//
+
+import UIKit
+
+class addMoneyWalletCell: UITableViewCell {
+
+    @IBOutlet weak var lblAccountNo: UILabel!
+    @IBOutlet weak var lblBankName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
