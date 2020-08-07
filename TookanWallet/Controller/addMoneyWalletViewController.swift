1797db7fa3ce67a96c87d850100e0aeb8f130d90
satyam <satyam.kumar@jungleworks.com>
2020-08-07 12:52:11 +0530

    Started implemented another screen

--- TookanWallet/Controller/addMoneyWalletViewController.swift
+++ TookanWallet/Controller/addMoneyWalletViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class addMoneyWalletViewController: UIViewController {
 
+    @IBOutlet weak var lblMoneyValue: UILabel!
+    @IBOutlet weak var lblAddMoneyWallet: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        lblAddMoneyWallet.text = " Add Money Wallet"
+        lblMoneyValue.text = "Rs .6000"
+        lblAddMoneyWallet.font = UIFont.boldSystemFont(ofSize: 20)
+        lblMoneyValue.font  = UIFont.boldSystemFont(ofSize: 20)
+        lblAddMoneyWallet.adjustsFontSizeToFitWidth = true
+        //lblMoneyValue.adjustsFontSizeToFitWidth = true
+        //lblAddMoneyWallet.adjustsFontForContentSizeCategory = true
         // Do any additional setup after loading the view.
     }
     
