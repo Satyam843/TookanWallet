6590a23ad0238c5cd5ffe74ea7886f4729ed15e0
satyam <satyam.kumar@jungleworks.com>
2020-08-08 12:44:53 +0530

    add Money Completed

--- TookanWallet/Controller/addMoneyWalletViewController.swift
+++ TookanWallet/Controller/addMoneyWalletViewController.swift
@@ -11,6 +11,17 @@ import UIKit
 class addMoneyWalletViewController: UIViewController {
     @IBOutlet weak var btnProceed: UIButton!
     
+    @IBOutlet weak var btnAddCard: UIButton!
+    @IBOutlet weak var btnIcici: UIButton!
+    @IBOutlet weak var btnHdfc: UIButton!
+    @IBOutlet weak var btnSbi: UIButton!
+    @IBOutlet weak var lblAddCard: UILabel!
+    @IBOutlet weak var lblIciciAccount: UILabel!
+    @IBOutlet weak var lblIciciBank: UILabel!
+    @IBOutlet weak var lblHdfcAccount: UILabel!
+    @IBOutlet weak var lblHdfcBank: UILabel!
+    @IBOutlet weak var lblSbiAccount: UILabel!
+    @IBOutlet weak var lblSbiBank: UILabel!
     @IBOutlet weak var addMoneyWalletTableView: UITableView!
     @IBOutlet weak var lblMoneyValue: UILabel!
     @IBOutlet weak var lblAddMoneyWallet: UILabel!
@@ -28,7 +39,39 @@ class addMoneyWalletViewController: UIViewController {
         //lblAddMoneyWallet.adjustsFontForContentSizeCategory = true
         // Do any additional setup after loading the view.
         btnProceed.titleLabel?.text = "Procced"
+        lblSbiBank.text = "State Bank of India"
+        lblSbiAccount.text = "A/c No . *****56847"
+        lblSbiBank.font = UIFont.boldSystemFont(ofSize: 20)
+        lblHdfcBank.text = "HDFC Bank"
+        lblHdfcAccount.text = "A/c No . *****56847"
+        lblHdfcBank.font = UIFont.boldSystemFont(ofSize: 20)
+        lblIciciBank.text = "ICICI Bank"
+        lblIciciAccount.text = "A/c No . *****56847"
+        lblIciciBank.font = UIFont.boldSystemFont(ofSize: 20)
+        lblAddCard.text = "Add Card"
+        lblAddCard.font = UIFont.boldSystemFont(ofSize: 20)
     }
 
+    @IBAction func tapRadioButton(_ sender: UIButton) {
+        if sender.tag == 1
+        {
+            btnSbi.isSelected = true
+            btnHdfc.isSelected = false
+            btnIcici.isSelected = false
+        }
+        else if sender.tag == 2
+        {
+            btnSbi.isSelected = false
+            btnHdfc.isSelected = true
+            btnIcici.isSelected = false
+        }
+        else if sender.tag == 3
+        {
+            btnSbi.isSelected = false
+            btnHdfc.isSelected = false
+            btnIcici.isSelected = true
+            
+        }
+    }
 }
 
